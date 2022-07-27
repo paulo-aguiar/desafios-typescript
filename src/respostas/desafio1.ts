ee085fa95969b25c955d5b1e011fe65e4606b7dd
paulo-aguiar <phaguiarrocha@gmail.com>
2022-07-26 21:59:38 -0300

    desafio 2

--- src/respostas/desafio1.ts
+++ src/respostas/desafio1.ts
@@ -1,6 +1,7 @@
 //  como podemos rodar isso em um arquivo .ts sem causar erros?
 
-let empregados: { codigo: number; nome: string } = {
+
+let empregados: {codigo: number, nome: string} = {
   codigo: 10,
-  nome: "Lucas"
-} 
+  nome: 'Lucas'
+};
